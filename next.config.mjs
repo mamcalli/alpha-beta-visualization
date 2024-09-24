824838a003aa649d9cb79a941852334c53ccd57f
mic <micmcal@icloud.com>
2024-09-24 00:47:43 -0500

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/alpha-beta-viz' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/alpha-beta-viz' : '',
-    images: {
-      unoptimized: true,
-    },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/alpha-beta-visualization' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
